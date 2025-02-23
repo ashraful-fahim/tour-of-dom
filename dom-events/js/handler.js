560711727fb926fd8c4eb364be93df290dd7a02d
ashraful-fahim <ashraful30june@gmail.com>
2025-02-23 23:19:20 +0600

    changing elements by clicking through event handling add listener component

---
+++ dom-events/js/handler.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-update-title')
+    .addEventListener('click', function () {
+        const pageTitle = document.getElementById('page-title');
+        pageTitle.innerText = 'Updated Event Handler by JS';
+    })
+
+
+//1. Set Event Listener
+document.getElementById('btn-update')
+    .addEventListener('click', function () {
+        console.log('Update Button Clicked!')
+        //2. Get the text from the Input Field
+        const nameInput = document.getElementById('name-input');
+        //console.log(nameInput);
+        const name = nameInput.value;
+        //console.log('name is', name);
+        const namePara = document.getElementById('name');
+        namePara.innerText = name;
+    })
